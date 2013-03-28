52da115ead41e7b3e19ce3e36f8386625fe7e813
Matt Nicklay <matthew.nicklay@gmail.com>
2013-03-28 01:24:33 -0300

    Update pyngram.py

--- pyngram.py
+++ pyngram.py
@@ -14,34 +14,33 @@ def get_ngrams(thestr):
     s = ' '.join(thestr.split()).split(' ')
     return [tuple(s[i:i+n]) for i in range(len(s)-n+1)]
 
-if __name__ == '__main__':
-    print 'reading in the data'
-    infile = open('inputfile.txt', 'r')
-    datarows = infile.readlines()
-    infile.close()
+print 'reading in the data'
+infile = open('inputfile.txt', 'r')
+datarows = infile.readlines()
+infile.close()
 
-    for n in [1, 2, 3]:
-        pool = Pool(processes=cpu_count())
-        go = GoGadgetGo()
+for n in [1, 2, 3]:
+    pool = Pool(processes=cpu_count())
+    go = GoGadgetGo()
 
-        print 'building full ngram list'
-        # Make a shared counter that all processes can use
-        counter = Counter()
-        # Chunk the data into 40000 rows per process at one time. This can be optimized
-        N = 40000
-        for i in range(len(datarows)//N+1):
-            g2 = pool.imap(get_ngrams, islice(go, N))
-            for ngrams in g2:
-                for ngram in ngrams:
-                    counter.update({' '.join(ngram): 1})
+    print 'building full ngram list'
+    # Make a shared counter that all processes can use
+    counter = Counter()
+    # Chunk the data into 40000 rows per process at one time. This can be optimized
+    N = 40000
+    for i in range(len(datarows)//N+1):
+        g2 = pool.imap(get_ngrams, islice(go, N))
+        for ngrams in g2:
+            for ngram in ngrams:
+                counter.update({' '.join(ngram): 1})
 
-        print 'sorting dict of ngram sums'
-        sorted_ngrams = sorted(counter.iteritems(), key=itemgetter(1), reverse=True)
+    print 'sorting dict of ngram sums'
+    sorted_ngrams = sorted(counter.iteritems(), key=itemgetter(1), reverse=True)
 
-        outfile = '%sgrams.txt' % n
-        f = open(outfile, 'w')
-        for gram in sorted_ngrams:
-            f.write('%s\t%s\n' % (gram[0], gram[1]))
-        f.close()
+    outfile = '%sgrams.txt' % n
+    f = open(outfile, 'w')
+    for gram in sorted_ngrams:
+        f.write('%s\t%s\n' % (gram[0], gram[1]))
+    f.close()
 
-        print 'COMPLETE:', datetime.now() - start_time
+    print 'COMPLETE:', datetime.now() - start_time
