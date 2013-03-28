c912bf558d5893efdbc486f242b1a0d14d8f26d2
Matt Nicklay <matthew.nicklay@gmail.com>
2013-03-28 19:39:07 -0300

    Update pyngram.py

--- pyngram.py
+++ pyngram.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from itertools import islice
 from multiprocessing import Pool,cpu_count
 from operator import itemgetter
+import sys
 
 
 def IterRows():
@@ -14,36 +15,40 @@ def get_ngrams(thestr):
     s = ' '.join(thestr.split()).split(' ')
     return [tuple(s[i:i+n]) for i in range(len(s)-n+1)]
 
+# Accept the input file from the arguments
+inputfile = sys.argv[1]
+
 print 'reading the data into memory'
-infile = open('inputfile.txt', 'r')
+infile = open(inputfile, 'r')
 datarows = infile.readlines()
 infile.close()
 
 # We'll use this later
 numrows = len(datarows)
 
-print 'calculate 1, 2 and 3 grams by default'
-for n in [1, 2, 3]:
-    pool = Pool(processes=cpu_count())
-    iterrows = IterRows()
-    print 'building full %sgram list' % n
-    
-    # Make a shared counter that all processes can use
-    counter = Counter()
-    
-    # Chunk the data into 40000 rows per process at one time. This can be optimized
-    N = 40000
-    for i in range(numrows//N+1):
-        g2 = pool.imap(get_ngrams, islice(iterrows, N))
-        for ngrams in g2:
-            for ngram in ngrams:
-                counter.update({' '.join(ngram): 1})
+# Take the value of n from the arguments
+n = int(sys.argv[2])
 
-    print 'writting sorted dict of ngram sums to disk'
-    outfile = '%sgrams.txt' % n
-    f = open(outfile, 'w')
-    for gram in sorted(counter.iteritems(), key=itemgetter(1), reverse=True):
-        f.write('%s\t%s\n' % (gram[0], gram[1]))
-    f.close()
+pool = Pool(processes=cpu_count())
+iterrows = IterRows()
+print 'building full %sgram list' % n
+    
+# Make a shared counter that all processes can use
+counter = Counter()
+    
+# Chunk the data into 40000 rows per process at one time. This can be optimized
+N = 40000
+for i in range(numrows//N+1):
+    g2 = pool.imap(get_ngrams, islice(iterrows, N))
+    for ngrams in g2:
+        for ngram in ngrams:
+            counter.update({' '.join(ngram): 1})
 
-    print 'COMPLETE:', datetime.now() - start_time
+print 'writting sorted dict of ngram sums to disk'
+outfile = '%sgrams.txt' % n
+f = open(outfile, 'w')
+for gram in sorted(counter.iteritems(), key=itemgetter(1), reverse=True):
+    f.write('%s\t%s\n' % (gram[0], gram[1]))
+f.close()
+
+print 'COMPLETE:', datetime.now() - start_time
